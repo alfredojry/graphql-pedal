46b972905ec1dfeec0c72dceaa96fcce05c43fa5
alfredo <yoelvisjimenezr@gmail.com>
2021-08-21 18:40:46 -0500

    configuraçao inicial para testes

---
+++ tests/funcional-tests.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import supertest from 'supertest';
+
+const url = 'http://localhost:4000';
+const request = supertest(url);
+
+describe('GraphQL', () => {
+    // Tests
+});
